cee021850b076bb7df11a7442a083628483ae9e7
CosmicKid <bartlomiej345@gmail.com>
2017-11-16 21:23:33 +0100

    Add simple form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,21 @@
 class UsersController < ApplicationController
   def new; end
 
-  def create; end
+  def create
+    @user = User.create(user_params)
+    if @user.save
+      redirect_to @user
+    else
+      render 'users/new'
+    end
+  end
 
   def show; end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
+  end
 end
