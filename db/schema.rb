f83b1b26f4e6e0dcd0b0e3d75954dc73907900e8
CosmicKid <bartlomiej345@gmail.com>
2017-11-20 18:53:28 +0100

    Add email index to users

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171120164537) do
+ActiveRecord::Schema.define(version: 20171120174816) do
 
   create_table "sessions", force: :cascade do |t|
     t.string "remember_digest"
@@ -24,6 +24,7 @@ ActiveRecord::Schema.define(version: 20171120164537) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
