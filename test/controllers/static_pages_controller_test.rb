1d351d0a6d018459e8118d9c0732c5ae43bba6df
Kamil <kamilszkaradnik@gmail.com>
2017-11-20 17:09:51 +0000

    Stronki mark2

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,16 +1,21 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+    
+    def setup
+    @base = "Ruby on Rails Tutorial Sample App"
+  end
+    
   test "should get FAQ" do
     get static_pages_FAQ_url
     assert_response :success
-     assert_select "title", "FAQ | LucidTracker"
+     assert_select "title", "FAQ | #{@base}"
   end
 
   test "should get Co_to" do
     get static_pages_Co_to_url
     assert_response :success
-     assert_select "title", "Co_to | LucidTracker"
+     assert_select "title", "Co_to | #{@base}"
   end
 
 end
