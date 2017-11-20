787e3323d8bc9536a42f34e5a76c0f12bbb67803
CosmicKid <bartlomiej345@gmail.com>
2017-11-20 18:42:29 +0100

    Add digest method

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,4 +11,12 @@ class User < ApplicationRecord
                        length: { minimum: 6 }
 
   before_save { email.downcase! }
+
+  class << self
+    def digest(string)
+      cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST
+                                                  : BCrypt::Engine.cost
+      BCrypt::Password.create(string, cost: cost)
+    end
+  end
 end
