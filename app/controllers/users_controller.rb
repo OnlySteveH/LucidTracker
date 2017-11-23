59fd03227f8cf21483cecc89bf1a24143ccd4cd4
Unknown <szeliga_d@o2.pl>
2017-11-23 23:16:59 +0100

    add controller and view to edit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,20 @@ class UsersController < ApplicationController
     @user = User.find_by(id: params[:id])
   end
 
+def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+ def update
+    @user = User.find_by(id: params[:id])
+    if @user.update_attributes(user_params)
+      flash[:success] = 'You signed up successfully'
+      redirect_to @user
+    else
+      render 'users/edit'
+    end
+  end
+
   private
 
   def user_params
