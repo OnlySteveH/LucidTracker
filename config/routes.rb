a8d8fc3834ea1c0e3bf27922d71d533faa3715ed
Kamil <kamilszkaradnik@gmail.com>
2017-11-24 15:05:36 +0000

    New bar

--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,15 @@
 Rails.application.routes.draw do
-
   
-
-  get '/signup', to: 'users#new'
-  post '/signup', to: 'users#create'
+  root   'static_pages#strona'
   
-<<<<<<< HEAD
   get  '/faq', to: 'static_pages#faq'
   get  '/co_to', to: 'static_pages#co_to'
   get '/spis', to: 'static_pages#spis'
-
-  get '/strona', to:'static_pages#strona'
   
-
-=======
+  get '/signup', to: 'users#new'
+  post '/signup', to: 'users#create'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
->>>>>>> master
-
   resources :users
 end
