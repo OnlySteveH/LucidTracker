f2bb97f5aa113abde801017eee5e4219b2e171dd
CosmicKid <barteksonic@gmail.com>
2017-11-26 19:41:05 +0100

    Add activation check during login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,14 @@ class SessionsController < ApplicationController
   def create
     @user = User.find_by(email: params[:session][:email].downcase)
     if @user && @user.authenticate(params[:session][:password])
-      log_in @user, remember: params[:session][:remember]
-      render 'new'
+      if @user.activated?
+        log_in @user, remember: params[:session][:remember]
+        render 'new'
+      else
+        flash[:warning] = 'Account not activated.'\
+                          'Check your email for the activation link'
+        redirect_to root_url
+      end
     else
       flash.now[:danger] = 'Invalid email/password combination'
       render 'new'
